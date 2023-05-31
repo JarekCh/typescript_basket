f349554d431c58617eab30ef4f57567988b509f1
Jaroslaw Charchula <charchula@gmail.com>
2023-05-31 22:05:24 +0200

    add fetching with useEffect

--- src/context/ProductProvider.tsx
+++ src/context/ProductProvider.tsx
@@ -6,6 +6,7 @@ export type ProductType = {
   price: number;
 };
 
+//const initState: ProductType[] = []
 const initState: ProductType[] = [
   {
     sku: 'item0001',
@@ -34,6 +35,21 @@ type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
   const [products, setProducts] = useState<ProductType[]>(initState);
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch('http://localhost:3500/products')
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+
+    fetchProducts().then((products) => setProducts(products));
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ products }}>
