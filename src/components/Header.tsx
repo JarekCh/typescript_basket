7aad80f8408b4e97df8971ab2dd2a1a041526dc0
Jaroslaw Charchula <charchula@gmail.com>
2023-06-18 23:59:14 +0200

    create Nav component add Header logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,20 @@
-const Header = () => {
+type PropsType = {
+  viewCart: boolean;
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Header = ({ viewCart, setViewCart }: PropsType) => {
+  const content = (
+    <header className='header'>
+      <div className='header__title-bar'>
+        <h1>Acme Co.</h1>
+        <div className='header__price-box'>
+          <p>Total Items:</p>
+          <p>Totla Price:</p>
+        </div>
+      </div>
+    </header>
+  );
   return <div>Header</div>;
 };
 
